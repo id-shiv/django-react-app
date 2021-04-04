03cda64b587de65f8e6e155a110aa9ad479e8b4e
id-shiv <id.shiv.prasad@gmail.com>
2021-04-04 21:30:14 +0530

    theme switch code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import './css/bootstrap.min.css'
 import './css/custom.css'
 
+import { Grid, Typography, Paper } from "@material-ui/core"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+
 import Layout from './components/Layout/LayoutComponent'
 
 function App() {
+  // define theme
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark"
+    }
+  })
+
+  // render
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Grid container direction="column">
       <Layout />
-    </div>
+      </Grid>
+      </Paper>
+      </ThemeProvider>
   );
 }
 
