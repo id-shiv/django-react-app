3734ff3653293b8d6cd777f8ad047945666a8bc7
id-shiv <id.shiv.prasad@gmail.com>
2021-04-15 21:49:16 +0530

    backend cors headers

--- frontend/src/components/Application/Notifications.jsx
+++ frontend/src/components/Application/Notifications.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -10,62 +11,57 @@ import TableRow from "@material-ui/core/TableRow";
 
 import { ContentTitle, ContentBody } from "../BodyTemplate";
 
-const columns = [
-  {
-    id: "category",
-    label: "Category",
-    minWidth: 100,
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "message",
-    label: "Message",
-    minWidth: 250,
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "timestamp",
-    label: "Time\u00a0Stamp",
-    minWidth: 170,
-    format: (value) => value.toLocaleString("en-US"),
-  },
-];
-
-function createData(category, message, timestamp) {
-  return { category, message, timestamp };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354),
-  createData("China", "CN", 1403500365),
-  createData("Italy", "IT", 60483973),
-  createData("United States", "US", 327167434),
-  createData("Canada", "CA", 37602103),
-  createData("Australia", "AU", 25475400),
-  createData("Germany", "DE", 83019200),
-  createData("Ireland", "IE", 4857000),
-  createData("Mexico", "MX", 126577691),
-  createData("Japan", "JP", 126317000),
-  createData("France", "FR", 67022000),
-  createData("United Kingdom", "GB", 67545757),
-  createData("Russia", "RU", 146793744),
-  createData("Nigeria", "NG", 200962417),
-  createData("Brazil", "BR", 210147125),
-];
-
 const Notifications = ({ classes }) => {
+  // get and save notifications from backend
+  const [notifications, setNotifications] = useState([]);
+  // perform side effects (fetching data) with useEffect hook
+  useEffect(() => {
+    // get notifications from backend and save the state
+    const notificationItems = [
+      { category: "INFO", message: "notification 1", timestamp: "872987287" },
+    ];
+    // set the retrieved data to the state
+    setNotifications(notificationItems);
+  }, []);
+
+  // set the columns of the table to be displayed
+  const columns = [
+    {
+      id: "category",
+      label: "Category",
+      minWidth: 100,
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "message",
+      label: "Message",
+      minWidth: 250,
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "timestamp",
+      label: "Time\u00a0Stamp",
+      minWidth: 170,
+      format: (value) => value.toLocaleString("en-US"),
+    },
+  ];
+
+  // set the pagination details to display notifications
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
+  // handle the change of page
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
+  // handle the change of rows per page
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
 
+  // set the main content body
   const body = (
     <>
       <Paper className={classes.notifications__container}>
@@ -85,18 +81,13 @@ const Notifications = ({ classes }) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows
+              {notifications
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
+                .map((notification, index) => {
                   return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.timestamp}
-                    >
+                    <TableRow hover role="checkbox" tabIndex={-1} key={index}>
                       {columns.map((column) => {
-                        const value = row[column.id];
+                        const value = notification[column.id];
                         return (
                           <TableCell key={column.id} align={column.align}>
                             {column.format && typeof value === "number"
@@ -114,7 +105,7 @@ const Notifications = ({ classes }) => {
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+          count={notifications.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onChangePage={handleChangePage}
@@ -124,19 +115,7 @@ const Notifications = ({ classes }) => {
     </>
   );
 
-  // Traditional Javascript way
-  // const xhr = new XMLHttpRequest();
-  // const method = "GET";
-  // const url = "http://127.0.0.1:8000/application/notifications/1";
-  // const responseType = "json";
-
-  // xhr.responseType = responseType;
-  // xhr.open(method, url);
-  // xhr.onload = function () {
-  //   console.log(xhr.response);
-  // };
-  // xhr.send();
-
+  // return the main title + body
   return (
     <>
       <ContentTitle classes={classes} child={"Notifications"} />
